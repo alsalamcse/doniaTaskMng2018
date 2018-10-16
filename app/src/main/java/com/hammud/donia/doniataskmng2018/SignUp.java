1cce2c1954da6df54438d3ced9822fb705e9e91e
user <noor@danon.tzafonet.org.il>
2018-10-16 13:22:33 +0300

    finishhhhhhhhhh

--- app/src/main/java/com/hammud/donia/doniataskmng2018/SignUp.java
+++ app/src/main/java/com/hammud/donia/doniataskmng2018/SignUp.java
@@ -1,13 +1,23 @@
 package com.hammud.donia.doniataskmng2018;
 
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+        import android.os.Bundle;
 
-public class SignUp extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class SignUp extends AppCompatActivity
+{
+    FirebaseAuth auth;// to establish sign in sign up
+    FirebaseUser user;// user
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        auth=FirebaseAuth.getInstance();
+        user=auth.getCurrentUser();//
     }
 }
+
