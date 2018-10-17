edceea8e5633fd3e0ee347a71e57524d13b92982
user <noor@danon.tzafonet.org.il>
2018-10-17 08:57:01 +0300

    finishhhhhhhhhh

--- app/src/main/java/com/hammud/donia/doniataskmng2018/SignUp.java
+++ app/src/main/java/com/hammud/donia/doniataskmng2018/SignUp.java
@@ -1,13 +1,20 @@
 package com.hammud.donia.doniataskmng2018;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
         import android.os.Bundle;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class SignUp extends AppCompatActivity
 {
+    // 1. add auth to project
+    //2.
     FirebaseAuth auth;// to establish sign in sign up
     FirebaseUser user;// user
 
@@ -16,8 +23,23 @@ public class SignUp extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        //3.
         auth=FirebaseAuth.getInstance();
         user=auth.getCurrentUser();//
+    }
+    //4.
+    private void creatAcount(String Email,String passw){
+        auth.createUserWithEmailAndPassword(Email,passw).addOnCompleteListener(SignUp.this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task)
+            {
+                if (task.isSuccessful())
+                {
+                    Toast.makeText(SignUp.this,"Authemtication successful",Toast)
+                }
+
+            }
+        });
     }
 }
 
