667f217bc553186f245345d3e33412230a0ee03e
user <noor@danon.tzafonet.org.il>
2018-10-18 09:43:24 +0300

    finishhhhhhhhhh

--- app/src/main/java/com/hammud/donia/doniataskmng2018/SignUp.java
+++ app/src/main/java/com/hammud/donia/doniataskmng2018/SignUp.java
@@ -1,10 +1,15 @@
 package com.hammud.donia.doniataskmng2018;
 
+import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
         import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.common.data.DataHolder;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
@@ -13,8 +18,11 @@ import com.google.firebase.auth.FirebaseUser;
 
 public class SignUp extends AppCompatActivity
 {
-    // 1. add auth to project
-    //2.
+    private EditText etfirst, etlast, phone,etEmail,etPassword;
+    private Button btnSave;
+    private FirebaseUser user;
+    private  FirebaseAuth auth;
+
     FirebaseAuth auth;// to establish sign in sign up
     FirebaseUser user;// user
 
@@ -23,11 +31,37 @@ public class SignUp extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-        //3.
+        etfirst=findViewById(R.id.etfirst);
+        etlast=findViewById(R.id.etlast);
+        phone=findViewById(R.id.phone);
+        etEmail=findViewById(R.id.etEmail);
+        etPassword=findViewById(R.id.etPassword);
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                dataHandler();
+
+            }
+        });
         auth=FirebaseAuth.getInstance();
-        user=auth.getCurrentUser();//
-    }
-    //4.
+        user=auth.getCurrentUser();
+
+         }
+         private void dataHandler(){
+             boolean isk=true;
+             String email=etEmail.getText().toString();
+             String Password=etPassword.getText().toString();
+             String fName=etfirst.getText().toString();
+             String LName=etlast.getText().toString();
+             String Phone=phone.getText().toString();
+             boolean isok= false;
+             if (email)
+         }
+
+
+
+
     private void creatAcount(String Email,String passw){
         auth.createUserWithEmailAndPassword(Email,passw).addOnCompleteListener(SignUp.this, new OnCompleteListener<AuthResult>() {
             @Override
@@ -35,7 +69,12 @@ public class SignUp extends AppCompatActivity
             {
                 if (task.isSuccessful())
                 {
-                    Toast.makeText(SignUp.this,"Authemtication successful",Toast)
+                    Toast.makeText(SignUp.this,"Authemtication successful",Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+                else
+                {
+                    Toast.makeText(SignUp.this,"Authemtication failed."+task.getException().getMessage(),Toast.LENGTH_SHORT).show();
                 }
 
             }
