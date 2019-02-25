2aa7bedac269474d7a89e7f9e600e581cdda66ea
donia <donya1@danon.tzafonet.org,il>
2019-02-25 14:05:24 +0200

    finishhhh

---
+++ app/src/main/java/com/hammud/donia/doniataskmng2018/cleaningTools.java
@@ -0,0 +1,13 @@
+package com.hammud.donia.doniataskmng2018;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class cleaningTools extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cleaning_tools);
+    }
+}
