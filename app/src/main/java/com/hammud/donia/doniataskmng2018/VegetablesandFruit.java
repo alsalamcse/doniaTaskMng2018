39ac0d5a92ed80842834e9ffd33cf2c9f2fb3faf
donia <donya1@danon.tzafonet.org,il>
2019-03-03 12:23:17 +0200

    finishhhh

--- app/src/main/java/com/hammud/donia/doniataskmng2018/VegetablesandFruit.java
+++ app/src/main/java/com/hammud/donia/doniataskmng2018/VegetablesandFruit.java
@@ -2,8 +2,12 @@ package com.hammud.donia.doniataskmng2018;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
-public class VegetablesandFruit extends AppCompatActivity {
+public class VegetablesandFruit extends AppCompatActivity
+{
+    private Button btnApple,btnAporicot,btnBanana,btnCherry,btnCelementine,btnFannel,btnGrape,btnGuava;
+    private Button BtnMango,btnOrange,btnStrawberry,btnAubergine,btnAvocado,btnCarrot,btnEndive,btnLemon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/hammud/donia/doniataskmng2018/cleaningTools.java
+++ app/src/main/java/com/hammud/donia/doniataskmng2018/cleaningTools.java
@@ -2,8 +2,12 @@ package com.hammud.donia.doniataskmng2018;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
-public class cleaningTools extends AppCompatActivity {
+public class cleaningTools extends AppCompatActivity
+{
+    private Button btnToiletTissus,btnKitchenRoll,btnFacialTissus, btnFabricConditioner,btnDetergentCapsules,btnLiquindDetergent;
+    private Button btnDishwashing,btnMatches,btnPaper,btnMops,btnCarpetCleaners,btnToiletCleaners,btnAirFresheners,btnFeatherDuesters;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
