65dd6e145003161265a4998fac8daf5abc7ab8f0
doniataskman <doniahammud@gmail.com>
2019-03-13 08:58:17 +0200

    dfjljou;p

--- app/src/main/java/com/hammud/donia/doniataskmng2018/UpdateFreezerOwnerActivity.java
+++ app/src/main/java/com/hammud/donia/doniataskmng2018/UpdateFreezerOwnerActivity.java
@@ -6,21 +6,29 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class UpdateFreezerOwnerActivity extends AppCompatActivity {
+public class UpdateFreezerOwnerActivity extends AppCompatActivity
+{
     private EditText Value , Weight , Amount;
     private Button Save;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update_freezer_owner);
         Value = (EditText) findViewById(R.id.Value_ID);
         Weight = (EditText) findViewById(R.id.Wieght_ID);
         Amount = (EditText) findViewById(R.id.Amount_ID);
         Save = (Button) findViewById(R.id.Save_ID);
-        Save.setOnClickListener(new View.OnClickListener() {
+        Save.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void onClick(View v) {
-                if (Value.getText().length() != 0 && Weight.getText().length() != 0 && Amount.getText().length() != 0){
+            public void onClick(View v)
+            {
+                if (Value.getText().length() != 0 && Weight.getText().length() != 0 && Amount.getText().length() != 0)
+                {
+                    String newVal=Value.getText().toString();
+                    Value.setText(newVal);
+
 
                 }
             }
